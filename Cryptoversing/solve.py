6a3ae5b28ea298751f8c318d84c49623605851b5
jerry <jerryzhang19257@gmail.com>
2022-12-22 14:49:50 -0700

    reorg

--- Cryptoversing/solve.py
+++ Cryptoversing/solve.py
@@ -7,7 +7,7 @@ base - 0x80 : Encrypted Input
 base - 0xa0 : Encrypted Flag 
 base - 0xa8 : Arr_1 -> int[2]{0x0, strlen/2}
 base - 0xb0 : Arr_2 -> int[2]{strlen, strlen/2}
-base - 0xb4 : Arr_3 -> int[2]{0x10, 0x18} <<< XOR first half string and second half string
+base - 0xb8 : Arr_3 -> int[2]{0x10, 0x18} <<< XOR first half string and second half string
 ''' 
 
 flag = 'h_bO}EcDOR+G)uh(jl,vL' # <<< Get from x/s $rbp-0xa0
